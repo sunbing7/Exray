bc5d6c37b4952bd9834b2abb43b93118425603ab
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-06-18 16:01:03 +0800

    test

--- abs_pytorch1_both_batch_inner_both_with_prune.py
+++ abs_pytorch1_both_batch_inner_both_with_prune.py
@@ -2199,7 +2199,9 @@ def mask_prune_by_neuron_mask(fxs, fys, fxs2, base_label, optz_label, model, chi
             else:
                 print('error data_option', ndata_option)
                 sys.exit()
-
+            print('[DEBUG]nuse_mask.shape'.format(nuse_mask.shape))
+            print('[DEBUG]ninner_data_1.shape'.format(ninner_data_1.shape))
+            print('[DEBUG]ninner_data_2.shape'.format(ninner_data_2.shape))
             mixed_data_1 = nuse_mask * ninner_data_2 + (1-nuse_mask) * ninner_data_1
             mixed_data_2 = nuse_mask * ninner_data_1 + (1-nuse_mask) * ninner_data_2
             if device == 'cuda':
